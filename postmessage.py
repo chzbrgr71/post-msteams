f8c1cdacf02f09edd9f9e120ceadaa7bc1518371
Brian Redmond <brianisrunning@gmail.com>
2018-11-29 13:37:21 -0800

    added JSON parsing

--- postmessage.py
+++ postmessage.py
@@ -1,12 +1,20 @@
 import pymsteams
 import sys
 import os
+import json
 
 # You must create the connectorcard object with the Microsoft Webhook URL
 myTeamsMessage = pymsteams.connectorcard(os.environ['TEAMS_WEBHOOK_URL'])
 
-# Add text to the message.
-myTeamsMessage.text(os.environ['TEAMS_MESSAGE'])
+with open('./event.json', 'r') as read_file:
+    data = json.load(read_file)
+
+message = 'A Github issue comment was posted. ' + data['comment']['body']
+
+# Add text to the message
+myTeamsMessage.text(message)
+myTeamsMessage.addLinkButton('Link to Github comment', data['issue']['html_url'])
+
 
 # send the message.
 myTeamsMessage.send()
